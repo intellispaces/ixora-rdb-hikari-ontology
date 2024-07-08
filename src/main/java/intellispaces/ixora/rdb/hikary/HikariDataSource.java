4bfe6a7f72af8e72d4a41fbedfa740d5e312c33e
lavor <lavor.andrey@gmail.com>
2024-07-08 12:50:47 +0600

    Initial commit

---
+++ src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSource.java
@@ -0,0 +1,8 @@
+package intellispaces.ixora.rdb.hikary;
+
+import tech.intellispaces.framework.core.annotation.Domain;
+import intellispaces.ixora.rdb.DataSource;
+
+@Domain("01908c4f-942b-7715-a5a0-0d89c9aaebaf")
+public interface HikariDataSource extends DataSource {
+}
