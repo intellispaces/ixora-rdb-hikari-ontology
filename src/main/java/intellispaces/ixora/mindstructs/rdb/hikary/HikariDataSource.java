7122e0dd0631efbd7f48547dc47fc5e7d5f80eee
lavor <lavor.andrey@gmail.com>
2024-07-22 09:11:57 +0600

    Rename to mindstructs

---
+++ src/main/java/intellispaces/ixora/mindstructs/rdb/hikary/HikariDataSource.java
@@ -0,0 +1,12 @@
+package intellispaces.ixora.mindstructs.rdb.hikary;
+
+import tech.intellispaces.framework.core.annotation.Domain;
+import intellispaces.ixora.mindstructs.rdb.DataSource;
+import tech.intellispaces.framework.core.annotation.Transition;
+
+@Domain("01908c4f-942b-7715-a5a0-0d89c9aaebaf")
+public interface HikariDataSource extends DataSource {
+
+//  @Transition("4167089f-3759-4187-85fb-5b324fcc2565")
+//  HikariDataSourceProperties properties();
+}
--- src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSource.java
+++
@@ -1,8 +0,0 @@
-package intellispaces.ixora.rdb.hikary;
-
-import tech.intellispaces.framework.core.annotation.Domain;
-import intellispaces.ixora.rdb.DataSource;
-
-@Domain("01908c4f-942b-7715-a5a0-0d89c9aaebaf")
-public interface HikariDataSource extends DataSource {
-}
