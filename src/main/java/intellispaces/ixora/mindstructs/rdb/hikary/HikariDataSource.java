3fadfc19ab0c0661d90857e1948549cb920d4bc8
lavor <lavor.andrey@gmail.com>
2024-07-25 10:57:25 +0600

    Minor change

--- src/main/java/intellispaces/ixora/mindstructs/rdb/hikary/HikariDataSource.java
+++ src/main/java/intellispaces/ixora/mindstructs/rdb/hikary/HikariDataSource.java
@@ -7,6 +7,6 @@ import tech.intellispaces.framework.core.annotation.Transition;
 @Domain("01908c4f-942b-7715-a5a0-0d89c9aaebaf")
 public interface HikariDataSource extends DataSource {
 
-//  @Transition("4167089f-3759-4187-85fb-5b324fcc2565")
-//  HikariDataSourceProperties properties();
+  @Transition("4167089f-3759-4187-85fb-5b324fcc2565")
+  HikariDataSourceProperties properties();
 }
